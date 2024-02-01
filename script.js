fa608672103b8543cf3baef265a3453d133adc81
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-02-02 00:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+	 const charMap = new Map();
+
+    for(let char of str){
+        if(charMap.has(char)){
+            charMap.set(char, charMap.get(char)+1);
+        }
+        else{
+            charMap.set(char, 1);
+        }
+    }
+
+    for(let char of str){
+        if(charMap.get(char) === 1){
+            return char;
+        }
+    }
+
+    return null;
+   }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
